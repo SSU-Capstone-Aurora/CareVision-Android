d36359c66ef753b0cc3bf7048a3b4f4654b3a8c8
kangyuri1114 <yuri3334@gmail.com>
2024-09-19 00:56:57 +0900

    [Feat] set typo

--- app/src/main/java/com/aurora/carevision/ui/theme/Type.kt
+++ app/src/main/java/com/aurora/carevision/ui/theme/Type.kt
@@ -1,34 +1,177 @@
 package com.aurora.carevision.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.aurora.carevision.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+
+val pretendardSemiBold = FontFamily(Font(R.font.pretendard_semibold, FontWeight.SemiBold))
+val pretendardRegular = FontFamily(Font(R.font.pretendard_regular, FontWeight.Normal))
+
+
+@Stable
+class CVTypography internal constructor(
+    head1: TextStyle,
+    head2: TextStyle,
+    head3: TextStyle,
+    body1: TextStyle,
+    body2: TextStyle,
+    body3: TextStyle,
+    body4: TextStyle,
+    caption1: TextStyle,
+    caption2: TextStyle,
+    caption3: TextStyle,
+    caption4: TextStyle,
+) {
+    var head1: TextStyle by mutableStateOf(head1)
+        private set
+    var head2: TextStyle by mutableStateOf(head2)
+        private set
+    var head3: TextStyle by mutableStateOf(head3)
+        private set
+    var body1: TextStyle by mutableStateOf(body1)
+        private set
+    var body3: TextStyle by mutableStateOf(body3)
+        private set
+    var body2: TextStyle by mutableStateOf(body2)
+        private set
+    var body4: TextStyle by mutableStateOf(body4)
+        private set
+    var caption1: TextStyle by mutableStateOf(caption1)
+        private set
+    var caption2: TextStyle by mutableStateOf(caption2)
+        private set
+    var caption3: TextStyle by mutableStateOf(caption3)
+        private set
+    var caption4: TextStyle by mutableStateOf(caption4)
+        private set
+
+    // Used when changing required properties
+    fun copy(
+        head1: TextStyle = this.head1,
+        head2: TextStyle = this.head2,
+        head3: TextStyle = this.head3,
+        body1: TextStyle = this.body1,
+        body2: TextStyle = this.body2,
+        body3: TextStyle = this.body3,
+        body4: TextStyle = this.body4,
+        caption1: TextStyle = this.caption1,
+        caption2: TextStyle = this.caption2,
+        caption3: TextStyle = this.caption3,
+        caption4: TextStyle = this.caption4,
+    ): CVTypography = CVTypography(
+        head1,
+        head2,
+        head3,
+        body1,
+        body2,
+        body3,
+        body4,
+        caption1,
+        caption2,
+        caption3,
+        caption4
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+
+    fun update(other: CVTypography) {
+        head1 = other.head1
+        head2 = other.head2
+        head3 = other.head3
+        body1 = other.body1
+        body2 = other.body2
+        body3 = other.body3
+        body4 = other.body4
+        caption1 = other.caption1
+        caption2 = other.caption2
+        caption3 = other.caption3
+        caption4 = other.caption4
+    }
+}
+
+fun careVisionTextStyle(
+    fontFamily: FontFamily,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontSize: TextUnit,
+    lineHeight: TextUnit,
+): TextStyle = TextStyle(
+    fontFamily = fontFamily,
+    fontWeight = fontWeight,
+    fontSize = fontSize,
+    lineHeight = lineHeight,
+    lineHeightStyle = LineHeightStyle(
+        alignment = LineHeightStyle.Alignment.Center,
+        trim = LineHeightStyle.Trim.None
     )
-    */
 )
+
+@Composable
+fun careVisionTypography(): CVTypography {
+    return CVTypography(
+        head1 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 22.sp,
+            lineHeight = 33.sp
+        ),
+        head2 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 20.sp,
+            lineHeight = 30.sp
+        ),
+        head3 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 18.sp,
+            lineHeight = 27.sp
+        ),
+        body1 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 16.sp,
+            lineHeight = 24.sp
+        ),
+        body2 = careVisionTextStyle(
+            fontFamily = pretendardRegular,
+            fontSize = 16.sp,
+            lineHeight = 24.sp
+        ),
+        body3 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 14.sp,
+            lineHeight = 21.sp
+        ),
+        body4 = careVisionTextStyle(
+            fontFamily = pretendardRegular,
+            fontSize = 14.sp,
+            lineHeight = 21.sp
+        ),
+        caption1 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 12.sp,
+            lineHeight = 18.sp
+        ),
+        caption2 = careVisionTextStyle(
+            fontFamily = pretendardRegular,
+            fontSize = 12.sp,
+            lineHeight = 18.sp
+        ),
+        caption3 = careVisionTextStyle(
+            fontFamily = pretendardSemiBold,
+            fontSize = 10.sp,
+            lineHeight = 15.sp
+        ),
+        caption4 = careVisionTextStyle(
+            fontFamily = pretendardRegular,
+            fontSize = 10.sp,
+            lineHeight = 15.sp
+        )
+    )
+}
