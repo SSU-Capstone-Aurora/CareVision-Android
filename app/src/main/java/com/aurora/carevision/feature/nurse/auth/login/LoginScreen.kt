6a86045e50a5fcdf442a182ee62937d4cada7142
kangyuri1114 <yuri3334@gmail.com>
2024-10-03 17:02:22 +0900

    add: nurse screen setting

---
+++ app/src/main/java/com/aurora/carevision/feature/nurse/auth/login/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.aurora.carevision.feature.nurse.auth.login
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen(){
+
+    // Login Screen
+}
---
+++ app/src/main/java/com/aurora/carevision/feature/nurse/auth/signup/SignUpScreen.kt
@@ -0,0 +1,9 @@
+package com.aurora.carevision.feature.nurse.auth.signup
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignUpScreen(){
+
+    // SignUp Screen
+}
