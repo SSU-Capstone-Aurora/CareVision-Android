eabd619806473d0c1bc4687b6f27e2807aecc351
kangyuri1114 <yuri3334@gmail.com>
2024-10-06 13:49:38 +0900

    feat: searchBar 구현

---
+++ app/src/main/java/com/aurora/carevision/core/component/CVSearchBar.kt
@@ -0,0 +1,105 @@
+package com.aurora.carevision.core.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.aurora.carevision.R
+import com.aurora.carevision.app.ui.theme.Black
+import com.aurora.carevision.app.ui.theme.CVTheme
+import com.aurora.carevision.app.ui.theme.Gray200
+import com.aurora.carevision.app.ui.theme.Gray500
+import com.aurora.carevision.app.ui.theme.White
+
+@Composable
+fun CVSearchBar(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier,
+    placeholder: String = "텍스트를 입력하세요",
+    backgroundColor: Color = White
+) {
+    val focusRequester = remember { FocusRequester() }
+
+    BasicTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = Modifier
+            .clip(shape = RoundedCornerShape(10.dp))
+            .background(backgroundColor)
+            .fillMaxWidth()
+            .padding(16.dp)
+            .focusRequester(focusRequester)
+            .onFocusChanged { focusState ->
+                if (focusState.isFocused) {
+                    focusRequester.requestFocus()
+                }
+            },
+        singleLine = true,
+        textStyle = CVTheme.typography.body1.copy(color = Gray500),
+        decorationBox = { innerTextField ->
+            Row(
+                verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_search_gray_24),
+                    contentDescription = "search icon",
+                    tint = Gray500,
+                )
+
+                Box(
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                ) {
+                    innerTextField()
+
+                    if (value.isEmpty()) {
+                        Text(
+                            text = placeholder,
+                            color = Gray500,
+                            style = CVTheme.typography.body1,
+                        )
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCVSearchBar() {
+    CVTheme {
+        Column(
+            modifier = Modifier.background(Black)
+        ) {
+            CVSearchBar(value = "", onValueChange = {}, modifier = Modifier)
+            CVSearchBar(
+                value = "dd",
+                onValueChange = {},
+                modifier = Modifier,
+                backgroundColor = Gray200
+            )
+        }
+    }
+}
