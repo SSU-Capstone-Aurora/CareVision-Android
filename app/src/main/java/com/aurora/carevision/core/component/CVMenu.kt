1b81d3488d335ac34dbcc439807499b89c2496d8
kangyuri1114 <yuri3334@gmail.com>
2024-10-09 13:33:01 +0900

    feat: menu 주석 해제

--- app/src/main/java/com/aurora/carevision/core/component/CVMenu.kt
+++ app/src/main/java/com/aurora/carevision/core/component/CVMenu.kt
@@ -1,14 +1,17 @@
 package com.aurora.carevision.core.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,8 +19,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.aurora.carevision.R
 import com.aurora.carevision.app.ui.theme.CVTheme
 import com.aurora.carevision.app.ui.theme.Primary300
 import com.aurora.carevision.app.ui.theme.White
@@ -29,15 +34,14 @@ fun ReviewDropdownMenu(
 ) {
     var isDropDownMenuExpanded by remember { mutableStateOf(true) }
 
-    // Trigger to open the dropdown menu
-//    IconButton(onClick = { isDropDownMenuExpanded = true }) {
-//        Image(
-//            painter = painterResource(id = R.drawable.ic_profile_frame_32),
-//            contentDescription = "menu icon",
-//            modifier = Modifier
-//                .size(36.dp)
-//        )
-//    }
+    IconButton(onClick = { isDropDownMenuExpanded = true }) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_profile_frame_32),
+            contentDescription = "menu icon",
+            modifier = Modifier
+                .size(36.dp)
+        )
+    }
 
     // Dropdown Menu
     DropdownMenu(
