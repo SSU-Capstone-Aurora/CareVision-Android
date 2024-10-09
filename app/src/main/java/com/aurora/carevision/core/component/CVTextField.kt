c8aa7d2d07ab14696d8d0fd5aea987864cefb9ea
kangyuri1114 <yuri3334@gmail.com>
2024-10-09 13:30:39 +0900

    feat: id textField 구현

--- app/src/main/java/com/aurora/carevision/core/component/CVTextField.kt
+++ app/src/main/java/com/aurora/carevision/core/component/CVTextField.kt
@@ -1,73 +1,60 @@
 package com.aurora.carevision.core.component
-import androidx.compose.foundation.Image
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aurora.carevision.R
+import com.aurora.carevision.app.ui.theme.Black
 import com.aurora.carevision.app.ui.theme.CVTheme
-import com.aurora.carevision.app.ui.theme.Gray100
-import com.aurora.carevision.app.ui.theme.Gray200
-import com.aurora.carevision.app.ui.theme.Gray300
-import com.aurora.carevision.app.ui.theme.Green300
-import com.aurora.carevision.app.ui.theme.Green400
-import com.aurora.carevision.app.ui.theme.Green500
+import com.aurora.carevision.app.ui.theme.Gray500
+import com.aurora.carevision.app.ui.theme.Primary600
+import com.aurora.carevision.app.ui.theme.Red600
 import com.aurora.carevision.app.ui.theme.White
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CVBasicTextField(
     value: String,
     placeholder: String,
+    label: String,
     onTextChanged: (String) -> Unit,
     onFocusChanged: (Boolean) -> Unit,
     modifier: Modifier = Modifier,
+    trailingIcon: Int? = null,
+    isError: Boolean = false,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions.Default,
 ) {
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
 
-    BasicTextField(
+    OutlinedTextField(
         value = value,
         onValueChange = onTextChanged,
+        isError = isError,
         modifier = modifier
             .clip(RoundedCornerShape(5.dp))
-            .background(Gray100)
-            .padding(horizontal = 10.dp, vertical = 10.dp)
             .focusRequester(focusRequester)
             .onFocusChanged { focusState ->
                 onFocusChanged(focusState.isFocused)
@@ -82,143 +69,70 @@ fun CVBasicTextField(
                 focusManager.clearFocus()
             }
         ),
-        textStyle = CVTheme.typography.textBody2Medium.copy(color = Gray100),
-        decorationBox = { innerTextField ->
-            Box (
-                modifier = Modifier.fillMaxWidth()
-
-
-            ) {
-                innerTextField()
-                if (value.isEmpty()) {
-                    Text(
-                        text = placeholder,
-                        textAlign = TextAlign.Start,
-                        color = Gray300,
-                        //modifier = Modifier.fillMaxWidth()
-                    )
-                }
-
+        textStyle = CVTheme.typography.textBody2Medium.copy(color = Black),
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Primary600,
+            unfocusedBorderColor = Gray500,
+            focusedLabelColor = Primary600,
+            focusedLeadingIconColor = Primary600,
+            focusedTrailingIconColor = Primary600,
+            unfocusedLabelColor = Gray500,
+            unfocusedLeadingIconColor = Gray500,
+            unfocusedTrailingIconColor = Gray500,
+            errorLabelColor = Red600,
+            errorBorderColor = Red600,
+            cursorColor = Primary600,
+        ),
+        label = {
+            Text(
+                text = label,
+                style = CVTheme.typography.captionRegular,
+                color = Gray500
+            )
+        },
+        placeholder = {
+            Text(
+                text = placeholder,
+                style = CVTheme.typography.captionRegular,
+                color = Gray500
+            )
+        },
+        trailingIcon = {
+            if (trailingIcon != null) {
+                Icon(painter = painterResource(id = trailingIcon), contentDescription = "")
             }
         }
     )
 }
-@Composable
-fun CVBasicInTextField(
-    value: String,
-    onTextChanged: (String) -> Unit,
-    onFocusChanged: (Boolean) -> Unit,
-    modifier: Modifier = Modifier,
-    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
-    keyboardActions: KeyboardActions = KeyboardActions.Default,
-) {
-    val focusRequester = remember { FocusRequester() }
-    val focusManager = LocalFocusManager.current
 
-    var isFocused by remember { mutableStateOf(false) }
-
-//    Column(
-//        modifier = modifier
-//            .fillMaxWidth()
-//    ) {
-
-        // BasicTextField 본문
-        Box(
-            modifier = Modifier
-                .clip(RoundedCornerShape(10.dp))
-                .border(2.5.dp, Green400, RoundedCornerShape(5.dp)) // 테두리 색상 설정
-                .background(Color.White) // 배경 색상 설정
-                .padding(horizontal = 10.dp, vertical = 10.dp)
-        ) {
-            Column (
-                modifier = modifier
-                    .fillMaxWidth()
-            )
-            {
-                if (value.isNotEmpty() || isFocused) {
-                    Text(
-                        text = "아이디",
-                        color = Gray300,
-                        style = CVTheme.typography.textBody2Medium,
-                        modifier = Modifier
-
-
-                    )
-                }
-                BasicTextField(
-                    value = value,
-                    onValueChange = onTextChanged,
-                    modifier = Modifier
-                        .clip(RoundedCornerShape(5.dp)) // textfield 모서리 둥글게..둥글게...
-                        .fillMaxWidth()
-                        .focusRequester(focusRequester)
-                        .onFocusChanged { focusState ->
-                            isFocused = focusState.isFocused
-                            onFocusChanged(focusState.isFocused)
-                        },
-                    singleLine = true,
-                    keyboardOptions = keyboardOptions,
-                    keyboardActions = KeyboardActions(
-                        onDone = {
-                            focusManager.clearFocus()
-                        },
-                        onSearch = {
-                            focusManager.clearFocus()
-                        }
-                    ),
-                    textStyle = CVTheme.typography.textBody1Medium.copy(color = Color.Black),
-                    decorationBox = { innerTextField ->
-                        Box(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-
-                            if (value.isEmpty() && !isFocused) {
-                                Text(
-                                    text = "aurora",
-                                    color = Gray300,
-                                    textAlign = TextAlign.Start,
-                                    style = CVTheme.typography.textBody2Medium
-                                )
-                            }
-                            innerTextField() // 실제 텍스트 필드
-                        }
-                    }
-                )
-            }
-
-        }
-    }
-//}
 @Preview
 @Composable
 fun CVTextFieldPreview() {
     CVTheme {
-        Column {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(White)
+        ) {
             CVBasicTextField(
                 value = "",
-                placeholder = "아이디를 입력해주세요",
+                placeholder = "아이디를 입력하세요",
+                label = "아이디",
                 onTextChanged = {},
                 onFocusChanged = {},
+                trailingIcon = R.drawable.ic_login_error_24
             )
 
             Spacer(modifier = Modifier.height(16.dp))
 
             CVBasicTextField(
                 value = "Text Entered",
-                placeholder = "Placeholder",
+                placeholder = "아이디를 입력하세요",
+                label = "아이디",
                 onTextChanged = {},
                 onFocusChanged = {},
+                isError = true
             )
-            Spacer(modifier = Modifier.height(16.dp))
-
-            CVBasicInTextField(
-                value = "aurora",
-                onTextChanged = {},
-                onFocusChanged = {},
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
         }
     }
 }
