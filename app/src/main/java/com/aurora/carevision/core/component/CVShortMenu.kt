f09da8da9c6fbd06f32efbc799d38d868bf4c367
kangyuri1114 <yuri3334@gmail.com>
2024-10-29 00:50:35 +0900

    feat: #21 CVShortDropDownMenu 생성

---
+++ app/src/main/java/com/aurora/carevision/core/component/CVShortMenu.kt
@@ -0,0 +1,94 @@
+package com.aurora.carevision.core.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.aurora.carevision.R
+import com.aurora.carevision.app.ui.theme.CVTheme
+import com.aurora.carevision.app.ui.theme.Gray500
+import com.aurora.carevision.app.ui.theme.Red700
+import com.aurora.carevision.app.ui.theme.White
+
+@Composable
+fun CVShortDropDownMenu(menuItems: List<String>, onMenuItemClick: (String) -> Unit) {
+    var isDropDownMenuExpanded by remember { mutableStateOf(false) }
+
+    Column(
+        horizontalAlignment = Alignment.End,
+        modifier = Modifier.wrapContentSize()
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_menu_button),
+            contentDescription = "menu icon",
+            modifier =
+            Modifier
+                .wrapContentSize()
+                .clickable { isDropDownMenuExpanded = true },
+            tint = Gray500
+        )
+
+        DropdownMenu(
+            expanded = isDropDownMenuExpanded,
+            onDismissRequest = { isDropDownMenuExpanded = false },
+            modifier = Modifier
+                .background(White)
+                .wrapContentSize()
+        ) {
+            menuItems.forEach { menuItem ->
+                DropdownMenuItem(
+                    onClick = {
+                        onMenuItemClick(menuItem)
+                        isDropDownMenuExpanded = false
+                    },
+                    text = {
+                        Text(
+                            text = menuItem,
+                            color = Red700,
+                            style = CVTheme.typography.textBody2Importance,
+                            textAlign = androidx.compose.ui.text.style.TextAlign.Center,
+                        )
+                    },
+                    modifier = Modifier
+                        .background(White)
+                        .fillMaxWidth()
+                )
+            }
+        }
+    }
+
+}
+
+@Composable
+@Preview
+fun CVShortDropDownMenuPreview() {
+    CVTheme {
+        Column(
+            modifier = Modifier
+                .background(White).fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val menuItems = listOf("ΕπΏψ")
+            CVShortDropDownMenu(menuItems) { selectedItem ->
+                println("Selected item: $selectedItem")
+            }
+        }
+    }
+}
