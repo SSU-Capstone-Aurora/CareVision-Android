7862f40ea883bbc4d84a1b05551a600857377852
kangyuri1114 <yuri3334@gmail.com>
2024-10-30 13:31:49 +0900

    [fix] #13 solve conflict

--- app/src/main/java/com/aurora/carevision/feature/admin/auth/login/AdminLoginScreen.kt
+++ app/src/main/java/com/aurora/carevision/feature/admin/auth/login/AdminLoginScreen.kt
@@ -1,13 +1,11 @@
 package com.aurora.carevision.feature.admin.auth.login
 
-import android.service.controls.Control
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,48 +18,42 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
-import androidx.navigation.NavHostController
-import androidx.navigation.Navigator
-import androidx.navigation.compose.rememberNavController
 import com.aurora.carevision.app.ui.theme.Black
 import com.aurora.carevision.app.ui.theme.CVTheme
 import com.aurora.carevision.app.ui.theme.Primary700
 import com.aurora.carevision.app.ui.theme.Red600
 import com.aurora.carevision.app.ui.theme.White
-import com.aurora.carevision.core.component.CVBasicButton
 import com.aurora.carevision.core.component.CVBasicTextField
 import com.aurora.carevision.core.component.CVLongButton
 import com.aurora.carevision.core.component.CVPasswordTextField
 import com.aurora.carevision.core.component.TopAppBarLeft
 
 @Composable
-fun AdminLoginScreen(){
+fun AdminLoginScreen() {
     var userID by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
-    var isError by remember{ mutableStateOf(false) }
-    var errorMessage by remember { mutableStateOf("")}
-    Column (
+    var isError by remember { mutableStateOf(false) }
+    var errorMessage by remember { mutableStateOf("") }
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(White)
             .padding(top = 52.dp, start = 24.dp, end = 24.dp)
-    ){
+    ) {
         TopAppBarLeft()
         Text(
             text = "안녕하세요 :) \n케어비전입니다",
             style = CVTheme.typography.headingPrimary,
             color = Color.Black,
             modifier = Modifier
-                .padding(top=16.dp, bottom = 24.dp)
+                .padding(top = 16.dp, bottom = 24.dp)
         )
         CVBasicTextField(
             value = userID,
             //isError = isError && userID != correctUserID,
             placeholder = "아이디를 입력해주세요",
             label = "아이디",
-            onTextChanged = {userID = it},
+            onTextChanged = { userID = it },
             onFocusChanged = {},
             modifier = Modifier
                 .fillMaxWidth()
@@ -72,14 +64,14 @@ fun AdminLoginScreen(){
             //isError = isError && password!= correctPassword,
             placeholder = "비밀번호를 입력해주세요",
             label = "비밀번호",
-            onTextChanged = {password = it},
+            onTextChanged = { password = it },
             onFocusChanged = {},
             modifier = Modifier
                 .padding(top = 24.dp)
                 .fillMaxWidth()
         )
 
-        if(isError){
+        if (isError) {
             Text(
                 text = errorMessage,
                 color = Red600,
@@ -121,15 +113,15 @@ fun AdminLoginScreen(){
 
 @Composable
 @Preview
-fun LoginScreenPreview(){
+fun LoginScreenPreview() {
     val correctUserID = "admin"
     val correctPassword = "password"
-    CVTheme{
+    CVTheme {
         Column(
             modifier = Modifier
                 .background(Black)
                 .fillMaxSize()
-        ){
+        ) {
             AdminLoginScreen()
         }
     }
