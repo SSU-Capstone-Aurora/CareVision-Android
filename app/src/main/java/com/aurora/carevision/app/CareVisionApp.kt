4af5372095a18605b6d474b4de14ef773bd50ccc
kangyuri1114 <yuri3334@gmail.com>
2024-11-02 20:13:07 +0900

    [feat] #27 CareVisionApp 생성

---
+++ app/src/main/java/com/aurora/carevision/app/CareVisionApp.kt
@@ -0,0 +1,11 @@
+package com.aurora.carevision.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CareVisionApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
