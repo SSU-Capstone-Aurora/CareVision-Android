8de0d5ea625ed2856f98a21da58ac3840da0657d
kangyuri1114 <yuri3334@gmail.com>
2024-11-08 00:26:01 +0900

    [feat] #34 라이브러리 세팅

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,8 +89,7 @@ dependencies {
 
     // retrofit
     implementation(libs.retrofit2)
-    // Json 데이터를 사용자가 정의한 Java 객체로 변환해주는 라이브러리
-    implementation(libs.retrofit2.converter.gson)
+    implementation(libs.retrofit2.kotlin.serialization)
 
     // okhttp : Retrofit으로 받는 데이터를 로그로 확인하기 위해
     implementation(libs.okhttp3)
