0ba2fac32f574909fd48c7700c90ae866f5f95b7
Woo02in <choiwoo02in@gmail.com>
2024-11-24 17:38:27 +0900

    feat: #47 Admin Signup API

--- app/src/main/java/com/aurora/carevision/data/remote/admin/auth/model/AdminSignUpResponse.kt
+++ app/src/main/java/com/aurora/carevision/data/remote/admin/auth/model/AdminSignUpResponse.kt
@@ -7,6 +7,13 @@ import kotlinx.serialization.Serializable
 data class AdminSignUpResponse(
     val id: Int = 0,
     val name: String = "",
+    val hospitalName: String = "",
+    val departmentName: String = ""
 )
 
-fun AdminSignUpResponse.toDomainModel() = AdminUser(id = id, name = name)
+fun AdminSignUpResponse.toDomainModel() = AdminUser(
+    id = id,
+    name = name,
+    hospitalName = hospitalName,
+    departmentName = departmentName
+)
