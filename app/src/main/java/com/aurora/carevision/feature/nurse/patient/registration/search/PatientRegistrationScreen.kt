9eb888b6e41a1d71cbd70c2531ab50707317e0b1
kangyuri1114 <yuri3334@gmail.com>
2024-11-30 14:05:44 +0900

    [feat] #62 연결되지 않은 환자 리스트 API 호출

--- app/src/main/java/com/aurora/carevision/feature/nurse/patient/registration/search/PatientRegistrationScreen.kt
+++ app/src/main/java/com/aurora/carevision/feature/nurse/patient/registration/search/PatientRegistrationScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -48,6 +49,10 @@ fun PatientRegistrationScreen(
 ) {
     var selectedPatientId by remember { mutableStateOf<Int?>(null) }
     val state = viewModel.state.collectAsState().value
+
+    LaunchedEffect(key1 = Unit) {
+        viewModel.getPatientList()
+    }
 
     Column(
         modifier = Modifier
